d0d3d00e86f2adf1e2a4fb0b9ca39150662f44a3
nishan <bhattarainisham@gmail.com>
2025-05-02 14:16:56 +0545

    Add Supabase initialization and app secrets import in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_proj/core/secrets/app_secrets.dart';
 import 'package:supabase_proj/core/theme/theme.dart';
 import 'package:supabase_proj/features/auth/presentation/pages/login_page.dart';
 import 'package:supabase_proj/features/auth/presentation/pages/signup_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Supabase
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseurl,
+    anonKey: AppSecrets.anonKey,
+  );
   runApp(const MyApp());
 }
 
