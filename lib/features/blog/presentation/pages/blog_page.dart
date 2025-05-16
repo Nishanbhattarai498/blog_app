9d1b80fdf49acbf18b058a2a09173b0c7f4d5ad2
nishan <bhattarainisham@gmail.com>
2025-05-16 18:24:15 +0545

    feat: convert BlogPage to StatefulWidget and fetch all blogs on initialization

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_proj/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:supabase_proj/features/blog/presentation/pages/add_new_blog_page.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends StatefulWidget {
   const BlogPage({super.key});
+
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Fetch all blogs when the page is initialized
+    context.read<BlogBloc>().add(BlogFetchAllBlog());
+  }
 
   @override
   Widget build(BuildContext context) {
